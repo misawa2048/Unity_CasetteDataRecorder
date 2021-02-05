d502c7e86365bbbcce5f63680e4c8db7e47f962f
misawa2048 <misawa@elix.jp>
2021-02-05 16:25:46 +0900

    update

--- Assets/Scripts/SquareWave.cs
+++ Assets/Scripts/SquareWave.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
 [RequireComponent(typeof(AudioSource))]
 public class SquareWave : MonoBehaviour
@@ -19,13 +20,15 @@ public class SquareWave : MonoBehaviour
     int bitPtr;
     byte[] m_fileBytes;
     int m_filePosPtr;
+    bool m_isStarted;
     bool m_isFinished;
+    AudioClip m_clip;
 
     void Start()
     {
         m_fileBytes = m_textAsset.bytes;
         m_filePosPtr = 0;
-        setFileDataOne();
+//        setFileDataOne(false);
         m_sqWaves = new float[samplerate * 2];
         for(int i=0;i< m_sqWaves.Length; ++i)
         {
@@ -36,11 +39,13 @@ public class SquareWave : MonoBehaviour
         pulseWCtr = 0;
         pulseSpd = 1;
         bitPtr = 0;
+        m_isStarted = false;
         m_isFinished = false;
-        AudioClip myClip = AudioClip.Create("MySinusoid", samplerate * 2, 1, samplerate, true, OnAudioRead, OnAudioSetPosition);
+        m_clip = AudioClip.Create("MySinusoid", samplerate * 2, 1, samplerate, true, OnAudioRead, OnAudioSetPosition);
         m_ac = GetComponent<AudioSource>();
-        m_ac.clip = myClip;
-        //m_ac.Play();
+        m_ac.clip = m_clip;
+        m_ac.Play();
+        StartCoroutine(GetDataCo());
     }
 
     private void Update()
@@ -68,7 +73,7 @@ public class SquareWave : MonoBehaviour
                 bitPtr++;
                 if(bitPtr>= bitDataArr.Length)
                 {
-                    if (setFileDataOne())
+                    if (setFileDataOne(m_isStarted))
                     {
                         m_isFinished = true;
                     }
@@ -83,15 +88,62 @@ public class SquareWave : MonoBehaviour
         position = newPosition;
     }
 
-    bool setFileDataOne()
+    bool setFileDataOne(bool _isStarted)
     {
-        Debug.Log(m_filePosPtr + "/" + m_fileBytes.Length);
-        byte dat = m_fileBytes[m_filePosPtr];
-        for(int i = 0; i < 8; ++i)
+        if (_isStarted)
         {
-            bitDataArr[1 + i] = ((dat >> i) & 1);
+            Debug.Log(m_filePosPtr + "/" + m_fileBytes.Length);
+            byte dat = m_fileBytes[m_filePosPtr];
+            bitDataArr[0] = 0;
+            for (int i = 0; i < 8; ++i)
+            {
+                bitDataArr[1 + i] = ((dat >> i) & 1);
+            }
+            bitDataArr[9] = 1;
+            bitDataArr[10] = 1;
+            m_filePosPtr = (m_filePosPtr + 1) % m_fileBytes.Length;
+            return (m_filePosPtr == 0);
         }
-        m_filePosPtr = (m_filePosPtr + 1) % m_fileBytes.Length;
-        return (m_filePosPtr == 0);
+        else
+        {
+            for (int i = 0; i < bitDataArr.Length; ++i)
+            {
+                bitDataArr[i] = 0;
+            }
+            return false;
+        }
+    }
+
+    IEnumerator GetDataCo()
+    {
+        Debug.Log("1");
+
+        UnityWebRequest www = new UnityWebRequest(m_url);
+        www.method = UnityWebRequest.kHttpVerbGET;
+        www.downloadHandler = new DownloadHandlerBuffer();
+
+        yield return www.SendWebRequest();
+
+        if (!string.IsNullOrEmpty(www.error))
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log("2:"+ www.responseCode);
+            if (www.responseCode == 200)
+            {
+                // UTF8•¶Žš—ñ‚Æ‚µ‚ÄŽæ“¾‚·‚é
+                string text = www.downloadHandler.text;
+
+                Debug.Log("!"+text);
+                m_fileBytes = www.downloadHandler.data;
+
+                Debug.Log("3");
+                m_isStarted = true;
+                m_ac.Play();
+            }
+        }
+        yield return null;
     }
 }
