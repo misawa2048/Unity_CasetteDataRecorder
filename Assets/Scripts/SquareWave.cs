d61a8141280b9019d0208e956d78682204cd7f34
misawa2048 <misawa@elix.jp>
2021-02-05 22:19:50 +0900

    update

--- Assets/Scripts/SquareWave.cs
+++ Assets/Scripts/SquareWave.cs
@@ -6,20 +6,18 @@ using UnityEngine.Networking;
 [RequireComponent(typeof(AudioSource))]
 public class SquareWave : MonoBehaviour
 {
-    [SerializeField] TextAsset m_textAsset = null;
-    [SerializeField] string m_url = "http://www.gutenberg.org/cache/epub/20781/pg20781.txt";
+    readonly int m_samplerate = 44100;
+    [SerializeField] float m_bps = 1200;
+    [SerializeField] InputField m_url = null;
     [SerializeField] Image m_progressImage = null;
+    int m_pulseW;
+    int m_pulseWCtr;
     AudioSource m_ac;
-    public int position = 0;
-    public int samplerate = 44100;
-    public float frequency = 440;
     float[] m_sqWaves;
-    int wavePtr;
-    int pulseW;
-    int pulseWCtr;
-    int pulseSpd;
-    int[] bitDataArr = { 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1 };
-    int bitPtr;
+    int m_wavePtr;
+    int m_m_pulseSpd;
+    int[] m_bitDataArr = { 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1 };
+    int m_bitPtr;
     byte[] m_fileBytes;
     int m_filePosPtr;
     int m_fileProgress;
@@ -29,31 +27,26 @@ public class SquareWave : MonoBehaviour
 
     void Start()
     {
-        m_fileBytes = m_textAsset.bytes;
-        m_filePosPtr = 0;
-        m_fileProgress = 0;
-        m_sqWaves = new float[samplerate * 2];
+        m_sqWaves = new float[m_samplerate * 2];
         for(int i=0;i< m_sqWaves.Length; ++i)
         {
-            m_sqWaves[i] = Mathf.Clamp(Mathf.Sin(2f * Mathf.PI * frequency * (float)i / samplerate) * 2f, -1f, 1f);
+            m_sqWaves[i] = Mathf.Clamp(Mathf.Sin(2f * Mathf.PI * m_bps * (float)i / m_samplerate) * 2f, -1f, 1f);
         }
-        wavePtr = 0;
-        pulseW = samplerate / (int)frequency;
-        pulseWCtr = 0;
-        pulseSpd = 1;
-        bitPtr = 0;
+        m_pulseW = m_samplerate / (int)m_bps;
+        m_pulseWCtr = 0;
+        m_filePosPtr = 0;
+        m_fileProgress = 0;
+        m_wavePtr = 0;
+        m_m_pulseSpd = 1;
+        m_bitPtr = 0;
         m_isStarted = false;
         m_isFinished = false;
-        m_clip = AudioClip.Create("MySinusoid", samplerate * 2, 1, samplerate, true, OnAudioRead, OnAudioSetPosition);
         m_ac = GetComponent<AudioSource>();
-        m_ac.clip = m_clip;
-        m_ac.Play();
-        StartCoroutine(GetDataCo());
     }
 
     private void Update()
     {
-        if (m_ac.isPlaying)
+        if (m_isStarted)
         {
             m_progressImage.fillAmount = (float)m_fileProgress / (float)m_fileBytes.Length;
             if (m_isFinished)
@@ -68,19 +61,29 @@ public class SquareWave : MonoBehaviour
         if (m_isFinished)
             return;
 
-        for(int i = 0; i < data.Length; ++i)
+        if (!m_isStarted)
         {
-            data[i] = m_sqWaves[wavePtr];
-            wavePtr = (wavePtr + pulseSpd) % m_sqWaves.Length;
-            pulseWCtr++;
-            if(pulseWCtr>= pulseW)
+            for (int i = 0; i < data.Length; ++i)
             {
-                pulseWCtr -= pulseW;
-                pulseSpd = bitDataArr[bitPtr]+1;
-                bitPtr++;
-                if(bitPtr>= bitDataArr.Length)
+                data[i] = m_sqWaves[m_wavePtr];
+                m_wavePtr = (m_wavePtr + 1) % m_sqWaves.Length;
+            }
+            return;
+        }
+
+        for (int i = 0; i < data.Length; ++i)
+        {
+            data[i] = m_sqWaves[m_wavePtr];
+            m_wavePtr = (m_wavePtr + m_m_pulseSpd) % m_sqWaves.Length;
+            m_pulseWCtr++;
+            if(m_pulseWCtr>= m_pulseW)
+            {
+                m_pulseWCtr -= m_pulseW;
+                m_m_pulseSpd = m_bitDataArr[m_bitPtr]+1;
+                m_bitPtr++;
+                if(m_bitPtr>= m_bitDataArr.Length)
                 {
-                    bitPtr -= bitDataArr.Length;
+                    m_bitPtr -= m_bitDataArr.Length;
                     if (setFileDataOne(m_isStarted))
                     {
                         m_isFinished = true;
@@ -93,7 +96,6 @@ public class SquareWave : MonoBehaviour
 
     void OnAudioSetPosition(int newPosition)
     {
-        position = newPosition;
     }
 
     bool setFileDataOne(bool _isStarted)
@@ -102,31 +104,50 @@ public class SquareWave : MonoBehaviour
         {
             //Debug.Log(m_fileProgress + "/" + m_fileBytes.Length);
             byte dat = m_fileBytes[m_filePosPtr];
-            bitDataArr[0] = 0;
+            m_bitDataArr[0] = 0;
             for (int i = 0; i < 8; ++i)
             {
-                bitDataArr[1 + i] = ((dat >> i) & 1);
+                m_bitDataArr[1 + i] = ((dat >> i) & 1);
             }
-            bitDataArr[9] = 1;
-            bitDataArr[10] = 1;
+            m_bitDataArr[9] = 1;
+            m_bitDataArr[10] = 1;
             m_filePosPtr = (m_filePosPtr + 1) % m_fileBytes.Length;
             m_fileProgress++;
         }
         else
         {
-            for (int i = 0; i < bitDataArr.Length; ++i)
+            for (int i = 0; i < m_bitDataArr.Length; ++i)
             {
-                bitDataArr[i] = 0;
+                m_bitDataArr[i] = 0;
             }
         }
         return (m_fileProgress > m_fileBytes.Length);
     }
 
+    public void OnCassetePlay()
+    {
+        OnCasseteStop();
+        m_clip = AudioClip.Create("MySinusoid", m_samplerate * 2, 1, m_samplerate, true, OnAudioRead, OnAudioSetPosition);
+        m_ac.clip = m_clip;
+        StartCoroutine(GetDataCo());
+    }
+
+    public void OnCasseteStop()
+    {
+        m_progressImage.fillAmount = 0;
+        m_isStarted = false;
+        m_isFinished = false;
+        m_fileProgress = 0;
+        m_ac.Stop();
+    }
+
+
     IEnumerator GetDataCo()
     {
-        Debug.Log("1");
+        m_ac.Stop();
+        m_ac.Play();
 
-        UnityWebRequest www = new UnityWebRequest(m_url);
+        UnityWebRequest www = new UnityWebRequest(m_url.text);
         www.method = UnityWebRequest.kHttpVerbGET;
         www.downloadHandler = new DownloadHandlerBuffer();
 
@@ -135,22 +156,20 @@ public class SquareWave : MonoBehaviour
         if (!string.IsNullOrEmpty(www.error))
         {
             Debug.Log(www.error);
+            OnCasseteStop();
         }
         else
         {
             Debug.Log("2:"+ www.responseCode);
             if (www.responseCode == 200)
             {
-                // UTF8•¶Žš—ñ‚Æ‚µ‚ÄŽæ“¾‚·‚é
-                string text = www.downloadHandler.text;
-
-                Debug.Log("!"+text);
                 m_fileBytes = www.downloadHandler.data;
                 Debug.Log("!" + m_fileBytes.Length);
-
-                Debug.Log("3");
                 m_isStarted = true;
-                m_ac.Play();
+            }
+            else
+            {
+                OnCasseteStop();
             }
         }
         yield return null;
